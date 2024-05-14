38fddff4bc369dad83530b1f151574354e399f72
Petr Balogh <pbalogh@redhat.com>
2024-05-14 14:11:18 +0200

    Add logic for dynamically get latest RPM for Noobaa SA

--- setup.py
+++ setup.py
@@ -12,12 +12,7 @@ setup(
         "Noobaa Standalone(SA) infra is used to deploy, upgrade and destroy NooBaa standalone on "
         "RHEL/CentOS stream platforms"
     ),
-    install_requires=[
-        "common-ci-utils",
-        "mergedeep",
-        "pynpm",
-        "pyyaml"
-    ],
+    install_requires=["common-ci-utils", "mergedeep", "pynpm", "pyyaml", "requests"],
     entry_points={
         "console_scripts": [
             "noobaa-sa-install=deployment.main:noobaa_sa_install",
